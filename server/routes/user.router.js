f792256ba8400745f538acdfe28afced10962211
Alex Goldberg <alexandergold53+github@gmail.com>
2021-09-08 14:41:38 -0600

    finished both PUT routes and finished My Account page

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -35,18 +35,35 @@ router.post('/register', (req, res, next) => {
 });
 
 // Handles PUT for updating user ADDRESS
-router.put('/update', (req, res, next) => {
+router.put('/updateaddress', (req, res, next) => {
+  let address = req.body.address;
+  let userid = req.body.userid;
 
-  const queryText = `INSERT INTO "user" (email, password, first_name, last_name, address, phone_number)
-    VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;`;
+  const queryText = `UPDATE "user" SET "address" = $1 WHERE "id" = $2;`;
   pool
-    .query(queryText, [username, password, firstname, lastname, address, phone])
+    .query(queryText, [address, userid])
     .then(() => res.sendStatus(201))
     .catch((err) => {
-      console.log('User registration failed: ', err);
+      console.log('Address update failed: ', err);
       res.sendStatus(500);
     });
 });
+
+// Handles PUT for updating user PHONE
+router.put('/updatephone', (req, res, next) => {
+  let phone = req.body.phone;
+  let userid = req.body.userid;
+
+  const queryText = `UPDATE "user" SET "phone_number" = $1 WHERE "id" = $2;`;
+  pool
+    .query(queryText, [phone, userid])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log('Address update failed: ', err);
+      res.sendStatus(500);
+    });
+});
+
 
 // Handles login form authenticate/login POST
 router.post('/login', userStrategy.authenticate('local'), (req, res) => {
--- src/components/MyAccount/MyAccount.jsx
+++ src/components/MyAccount/MyAccount.jsx
@@ -1,13 +1,13 @@
 import '../MyCoverLetters/MyCoverLetters.css'
 import '../MyAccount/MyAccount.css'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { TextField } from '@material-ui/core';
 import MaskedInput from 'react-text-mask';
-
+import axios from 'axios';
 
 const phoneStyles = makeStyles((theme) => ({
     root: {
@@ -42,6 +42,7 @@ const paperStyles = makeStyles((theme) => ({
 
 function MyAccount() {
     const userstore = useSelector(store => store.user);
+    const dispatch = useDispatch();
     const paperclasses = paperStyles();
     const bttnclasses = bttnStyles();
     const phoneclasses = phoneStyles();
@@ -50,7 +51,23 @@ function MyAccount() {
     const [address, setAddress] = React.useState("");
     const [phone, setPhone] = React.useState("");
 
-    console.log(userstore);
+    //POST to update ADDRESS
+    const updateAddress = () => {
+        axios.put(('/api/user/updateaddress'), {address: address, userid: userstore.id})
+        dispatch({
+            type: 'FETCH_USER'        
+        })
+    }
+
+    //POST to update PHONE
+    const updatePhone = () => {
+    axios.put(('/api/user/updatephone'), {phone: phone, userid: userstore.id})
+    dispatch({
+        type: 'FETCH_USER'        
+    })
+}
+
+
     return (
         <div>
             <div id="mycoverletter-container">
@@ -61,25 +78,23 @@ function MyAccount() {
                 <Paper elevation={3}>  
                     <h3 className="center">Account Details</h3>
                     <div id = "wrapper-div">
-                        <div id = 'account-info'>
-                            <p>Email: {userstore.email}</p> 
-                            <p>Address: {userstore.address}</p> 
-                            <TextField fullWidth />
-                            <Button variant="contained" color = "primary">Update Address</Button> 
-                            <p>Phone: {userstore.phone_number}</p> 
-                            <MaskedInput className={phoneclasses.root} 
-                                mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-                                placeholderChar={'\u2000'}
-                                showMask
-                                value={phone}
-                                required
-                                onChange={(event) => setPhone(event.target.value)} 
-                            />
-                            <Button variant="contained" color = "primary">Update Phone</Button> 
-
-
-                           
-                        </div>
+                        <form>
+                            <div id = 'account-info'>
+                                <p>Email: {userstore.email}</p> 
+                                <p>Address: {userstore.address}</p> 
+                                <TextField type="text" placeholder ="1234 Example St. Denver, CO 80204" fullWidth onChange={(e)=>setAddress(e.target.value)}/>
+                                <Button variant="contained" color = "primary" onClick={updateAddress} >Update Address</Button> 
+                                <p>Phone: {userstore.phone_number}</p> 
+                                <MaskedInput className={phoneclasses.root} 
+                                    mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+                                    placeholderChar={'\u2000'}
+                                    showMask
+                                    value={phone}
+                                    onChange={(event) => setPhone(event.target.value)} 
+                                />
+                                <Button variant="contained" color = "primary" onClick={updatePhone}>Update Phone</Button> 
+                            </div>
+                        </form>
                     </div>
                 </Paper> 
             </div>
