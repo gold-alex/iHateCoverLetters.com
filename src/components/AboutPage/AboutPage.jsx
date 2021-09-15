e9f6f8d156e7a60249ac13f9a07089b95b9b45bf
Alex Goldberg <alexandergold53+github@gmail.com>
2021-09-14 23:29:15 -0600

    finished basic about page features

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./AboutPage.css";
+import InsertBody from './Instructional_Gifs/InsertBody.gif'
+import PositionTag from './Instructional_Gifs/PositionTag.gif'
+import CompanyTag from './Instructional_Gifs/CompanyTag.gif'
+import TimeToCook from './Instructional_Gifs/TimeToCook.gif'
+
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -20,6 +25,20 @@ function AboutPage() {
             cover letter and iHateCoverLetters will create and bulk export clean,
             company specific cover letters in PDF format.
           </h5>
+          <h1 style={{marginTop: '50px'}}>Step 1: Insert bodies of text</h1>
+          <img src={InsertBody} />
+          <h1>Step 2: Add Company or Job Title Tags (Optional)</h1>
+          <h2>Job Title Tag *TITLE* </h2>
+          <img src={PositionTag} />
+          <h2>Company Tag *COMPANY*</h2>
+          <img style={{marginBottom: '50px'}} src={CompanyTag} />
+          
+          <h1>Step 3: Time to Cook!</h1>
+          <img style={{marginBottom: '75px'}} src={TimeToCook} />
+
+
+
+
       </div>
     </div>
   );
